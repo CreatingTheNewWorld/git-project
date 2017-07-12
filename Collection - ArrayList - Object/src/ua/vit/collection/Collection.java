09018dae92098abb0d7bb923c2a13202171e8b50
WorkUser <WorkUser@10.148.30.59>
2017-07-12 19:08:59 +0300

    created new java project

---
+++ Collection - ArrayList - Object/src/ua/vit/collection/Collection.java
@@ -0,0 +1,23 @@
+package ua.vit.collection;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Collection {
+	
+	private static ArrayList<Mobile> mobiles = new ArrayList<Mobile>();
+	private static Random r = new Random();
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<=300; i++){
+			mobiles.add(new Mobile(r.nextInt(9999999), "ssasdasdas"));
+		}
+		
+		for(Mobile m : mobiles){
+			System.out.println(m.getNumber() + " " + m.getName());
+		}
+		
+		
+	}
+
+}
